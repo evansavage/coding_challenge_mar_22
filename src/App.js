0bcf9701c7faed941584bcc90cf9ea4bd94cb6f4
evansavage <esavage@rapidflowtech.com>
2022-03-31 09:54:27 -0400

    Structural cleanup and deprecated component removal

--- src/App.js
+++ src/App.js
@@ -1,147 +1,69 @@
-import React, { useEffect, useState, useContext, useMemo, useRef } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "./App.css";
 import { getRandomWorks } from "./getRandomWorks";
 
-import ArtCard from "./ArtCard.js";
-import "./scss/App.scss";
+import SlideShow from "./SlideShow.js"
 
-import Flickity from 'react-flickity-component';
-import '../node_modules/flickity/css/flickity.css';
-
-import 'react-slideshow-image/dist/styles.css'
-
-import { Slide } from 'react-slideshow-image';
-
-function useTraceUpdate(props) {
-  const prev = useRef(props);
-  useEffect(() => {
-    const changedProps = Object.entries(props).reduce((ps, [k, v]) => {
-      if (prev.current[k] !== v) {
-        ps[k] = [prev.current[k], v];
-      }
-      return ps;
-    }, {});
-    if (Object.keys(changedProps).length > 0) {
-      console.log('Changed props:', changedProps);
-    } else {
-      console.log('no props changed');
-    }
-    prev.current = props;
-  });
-}
-
-const ThingsContext = React.createContext([{}, () => {}])
+// Imports from material UI
+import CircularProgress from '@mui/material/CircularProgress';
+import Button from '@mui/material/Button';
+import SaveIcon from '@mui/icons-material/Save';
+import RefreshIcon from '@mui/icons-material/Refresh';
 
 function App() {
 
+  // States for the random work objects, saved work objects, and the index of the current batch
   const [randomWorks, setRandomWorks] = useState([]);
-  const [batchLength, setBatchLength] = useState(10);
-  const savedWorks = useRef({});
+  const [savedWorks, setSavedWorks] = useState({});
   const [worksIter, setWorksIter] = useState(0);
+
+  // slideRef for referencing the nested SlideShow component
+  const slideRef = useRef(null);
 
   useEffect(() => {
 
-    getRandomWorks(batchLength).then(function(data) {
+    getRandomWorks().then(function(data) {
         setRandomWorks(data);
-        setBatchLength(data.length);
     });
 
-  }, [batchLength]);
+  }, []);
 
+  // Empties randomWorks to trigger loading render, then refills the state
   function handleWorksReload() {
     setRandomWorks([]);
-    getRandomWorks(batchLength).then(function(data) {
+    getRandomWorks().then(function(data) {
         setRandomWorks(data);
-        setBatchLength(data.length);
         setWorksIter(0);
     });
   }
 
-  const Carousel = React.memo(props => {
-    // useTraceUpdate(props);
-    let flktyRef;
+  // - Updates worksIter on save to ensure Controlled Component behavior of SlideShow on rerender
+  // - Checks worksSaved for current slide artwork id, adds to worksSaved if new
+  function handleWorksSave() {
+    let slideIndex = slideRef.current.state.index;
+    setWorksIter(slideIndex);
 
-    // let thingsContext = useContext(ThingsContext);
-    const randoms = props.randomWorks;
-    const setWorksIter = props.setWorksIter;
-    // let savedWorks = thingsContext.savedWorks;
-    // let setSavedWorks = thingsContext.setSavedWorks;
-    console.log(randomWorks);
-
-    const testWorksIter = useRef(0);
-    // const [savedWorks, setSavedWorks] = useState({});
-
-    function handleWorksSave() {
-      // console.log(randoms[testWorksIter.current].id in savedWorks);
-      // if ( !(randoms[testWorksIter.current].id in savedWorks) ) {
-      //   setSavedWorks({...savedWorks, [randoms[testWorksIter.current].id] : randoms[testWorksIter.current]});
-      // }
-      // console.log(savedWorks);
-      // setSavedWorks({'yes':'no'});
-      console.log('works saved')
+    if ( !(randomWorks[slideIndex].id in savedWorks) ) {
+      setSavedWorks({...savedWorks, [randomWorks[slideIndex].id]: randomWorks[slideIndex]});
     }
-
-    useEffect(() => {
-      const slideHandler = e => {
-        console.log(flktyRef.selectedIndex);
-        testWorksIter.current = flktyRef.selectedIndex;
-        console.log(testWorksIter.current);
-      }
-
-      flktyRef.on('change', slideHandler);
-      return () => {
-        flktyRef.off('change');
-      };
-
-    }, [flktyRef])
-
-
-    return (<>
-      <Flickity flickityRef={(fr) => flktyRef = fr} imagesLoaded={false} disableImagesLoaded={true} lazyLoad={true}>
-          {randoms.map((item, index) => <ArtCard {...item} batchLength={batchLength} key={item.id} index={index} />)}
-      </Flickity>
-      <button className="save-button" onClick={handleWorksSave}>Save work</button>
-    </>)
-  })
-
-  const SlideShow = (props) => {
-    useTraceUpdate(props);
-
-    const testWorksIter = useRef(0);
-
-    function handleWorksSave() {
-
-      if ( !(props.randomWorks[testWorksIter.current].id in props.savedWorks) ) {
-        props.savedWorks.current = {...props.savedWorks.current, [props.randomWorks[testWorksIter.current].id]: props.randomWorks[testWorksIter.current]}
-      }
-
-      console.log(props.savedWorks.current)
-    }
-
-    function handleSlideChange(oldIndex, newIndex) {
-      // console.log(oldIndex, newIndex);
-      testWorksIter.current = newIndex;
-    }
-
-    return (<>
-      <div className="slideshow-wrapper">
-        <Slide easing="ease" transitionDuration={400} onChange={handleSlideChange} autoplay={false}>
-          {props.randomWorks.map((item, index) => <ArtCard {...item} batchLength={batchLength} key={item.id} index={index} />)}
-        </Slide>
-      </div>
-      <button className="save-button" onClick={handleWorksSave}>Save work</button>
-    </>)
   }
 
   return (
     <div className="App">
         <header className="App-header">
           {randomWorks.length > 0
-            ? <SlideShow randomWorks={randomWorks} savedWorks={savedWorks}/>
-            : <div>loading</div>
+            ? <div className="overall-wrapper">
+              <SlideShow slideRef={slideRef} randomWorks={randomWorks} savedWorks={savedWorks} setSavedWorks={setSavedWorks} worksIter={worksIter} setWorksIter={setWorksIter} />
+              <div className="button-row">
+                <div>
+                  <Button style={{ color: 'white', borderColor: 'white' }} variant="outlined" className="save-button" onClick={handleWorksSave}><SaveIcon fontSize="large" /></Button>
+                  <div className="saved-total">{Object.keys(savedWorks).length > 0 ? Object.keys(savedWorks).length: ""}</div>
+                </div>
+                <Button style={{ color: 'white', borderColor: 'white' }} variant="outlined" className="reload-works" onClick={handleWorksReload}><RefreshIcon fontSize="large" /></Button>
+              </div>
+              </div>
+            : <CircularProgress style={{ color: 'white', borderColor: 'white' }} />
           }
-          <button className="reload-works" onClick={handleWorksReload}>Reload</button>
-          <div>{Object.keys(savedWorks).length - 1}</div>
         </header>
     </div>
   );
